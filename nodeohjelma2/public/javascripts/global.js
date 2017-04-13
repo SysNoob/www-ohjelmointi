c20fd3c362c42c7456470c9395af0c0af91f066b
Olavi Viitanen <olavi@debian>
2017-04-13 18:58:06 +0300

    Käyttäjän lisäys ja poisto, css viilaus

--- nodeohjelma2/public/javascripts/global.js
+++ nodeohjelma2/public/javascripts/global.js
@@ -9,6 +9,12 @@ $(document).ready(function() {
 	
 	// Käyttäjänimen linkin painaminen
 	$('#kayttajaLista table tbody').on('click', 'td a.linkkinaytakayttaja', naytaKayttajaInfo);
+
+	// Käyttäjän lisääminen
+	$('#btnLisaaKayttaja').on('click', lisaaKayttaja);
+	
+	// Käyttäjän poisto
+	$('#kayttajaLista table tbody').on('click', 'td a.linkkipoistakayttaja', poistaKayttaja);
 	
 });
 
@@ -64,4 +70,99 @@ function naytaKayttajaInfo(event) {
 	$('#kayttajaInfoSijainti').text(tamaKayttajaObjekti.sijainti);
 
 }
+
+
+// Käyttäjän lisääminen
+function lisaaKayttaja(event) {
+	
+	event.preventDefault();
+	
+	// Yksinkertainen validaatio
+	var virheMaara = 0;
+	$('#lisaaKayttaja input').each(function(indexm, val) {
+		if($(this).val() === '') {
+			virheMaara++;
+		}
+	});
+	
+	if(virheMaara === 0) {
+	
+		// Yhdistetään kaikki käyttäjäinfo yhdeksi objektiksi
+		var uusiKayttaja = {
+			'nimi': $('#lisaaKayttaja fieldset input#inputKayttajaNimi').val(),
+			'sposti': $('#lisaaKayttaja fieldset input#inputKayttajaSahkoposti').val(),
+			'kokonimi': $('#lisaaKayttaja fieldset input#inputKayttajaKokoNimi').val(),
+			'ika': $('#lisaaKayttaja fieldset input#inputKayttajaIka').val(),
+			'sijainti': $('#lisaaKayttaja fieldset input#inputKayttajaSijainti').val(),
+			'sukupuoli': $('#lisaaKayttaja fieldset input#inputKayttajaSukupuoli').val(),
+		}
+		
+		// Syötetään AJAXilla lisaakayttajaan
+		$.ajax({
+			type: 'POST',
+			data: uusiKayttaja,
+			url: '/users/lisaakayttaja',
+			dataType: 'JSON'
+		}).done(function(response) {
+		
+			// Tarkistetaan että vastaus on oikeellinen (tyhjä '')
+			if(response.msg === '') {
+				
+				// Tyhjennetään formi
+				$('#lisaaKayttaja fieldset input').val('');
+				
+				// Päivitetään taulukko
+				taytaTaulukko();
+			} else {
+			
+				// Virhetilanteessa palauta error viesti, jonka 'palvelu' antaa meille
+				alert('Error: ' + response.msg);
+				
+			}
+		});
+	
+	} else {
+		// Jos virheMaara > 0, tulosta virhetilanteen viesti
+		alert('Täytä kaikki kohdat, kiitos.');
+		return false;
+	}
+
+};
+
+
+// Poista Käyttäjä
+function poistaKayttaja(event) {
+	event.preventDefault();
+	
+	//Varmistetaan, että käyttäjä halutaan poistaa
+	var varmistus = confirm('Oletteko aivan varma, että haluatte poistaa käyttäjän?');
+	
+	if (varmistus === true) {
+	
+		$.ajax({
+			type: 'DELETE',
+			url: '/users/poistakayttaja/' + $(this).attr('rel')
+		}).done(function(response) {
+		
+			//Tarkistetaan vastauksen oikeellisuus (tyhjä '')
+			if(response.msg === '') {
+			
+			} else {
+			
+				alert('Error: ' + response.msg);
+			}
+			
+			//Päivitetään taulukko
+			taytaTaulukko();
+		});
+		
+	}else {
+	
+		// Ei tehdä mitään, jos käyttäjä ei halunnut poistaa
+		return false;
+	}
+}
+
+
+
 
--- nodeohjelma2/routes/users.js
+++ nodeohjelma2/routes/users.js
@@ -14,5 +14,29 @@ router.get('/kayttajalista', function(req, res) {
 
 });
 
+/*POST Lisää käyttäjä*/
+
+router.post('/lisaakayttaja', function(req, res) {
+
+	var db = req.db;
+	var tietokanta = db.get('käyttäjät');
+	tietokanta.insert(req.body, function(err, result){
+		res.send(
+			(err === null) ? { msg: ''} : { msg: err }
+		);
+	});
+});
+
+/* DELETE poistetaan käyttäjä*/
+
+router.delete('/poistakayttaja/:id', function(req, res) {
+	var db = req.db;
+	var tietokanta = db.get('käyttäjät');
+	var kayttajaPoisto = req.params.id;
+	tietokanta.remove({ '_id': kayttajaPoisto }, function(err) {
+		res.send((err === null) ? { msg: '' } : { msg:'Error: ' + err });
+	});
+});
+
 
 module.exports = router;
